3e4dcac1c5aa7e40cb57c958b7389ef2ab55c9cd
Bhuvan Raj <163994129+bhuvan-raj@users.noreply.github.com>
2025-08-25 23:38:16 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -16,14 +16,15 @@ terraform {
 }
 
 # Provision a Kind Cluster
+# Provision a Kind Cluster
 resource "null_resource" "kind_cluster" {
   provisioner "local-exec" {
-    command = "sudo sh -c 'kind create cluster --name my-gitops-cluster'"
+    command = "sudo sh -c '/home/bubu/bin/kind create cluster --name my-gitops-cluster'"
   }
 
   provisioner "local-exec" {
     when    = destroy
-    command = "sudo sh -c 'kind delete cluster --name my-gitops-cluster'"
+    command = "sudo sh -c '/home/bubu/bin/kind delete cluster --name my-gitops-cluster'"
   }
 }
 
